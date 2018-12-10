8c769ddbd08dc9beb8e8bbdbe56509fa2566fa45
Donald Liu <donaldliu21194@gmail.com>
2018-12-10 18:42:51 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,16 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts "Squawk!"
+  
+  return "Squawk!"
+end
+
+
+def parrot(string)
+  puts string
+  
+  return string
+
+end
